34383ecf3e924dd9add12114b454573f333c5e14
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2015-05-19 20:09:46 +0200

    get old behavior back with new flags and shit

--- moodle-destroyer.py
+++ moodle-destroyer.py
@@ -5,10 +5,24 @@ import argparse
 
 parser = argparse.ArgumentParser(prog="Moodle Destroyer", prefix_chars="-")
 
-parser.add_argument("-v", "--version", help="WE ARE TALKING ABOUT THE VERSION")
-parser.add_argument("-g", "--grading", help="ALL YOUR GRADINGS BELONG TO US")
-parser.add_argument("-m", "--moodle", help="gimme the moodle stuff")
-parser.add_argument("-r", "--result", help="name your result")
+parser.add_argument("-d", "--destroy",
+                    nargs=2,
+                    required=True,
+                    type=argparse.FileType('rU'),
+                    help="grading-file, moodle-file, result-file")
+parser.add_argument("-r", "--result",
+                    nargs=1,
+                    required=False,
+                    type=argparse.FileType('w'),
+                    help="result-file")
+parser.add_argument("-s", "--single",
+                    action="store_true",
+                    default=False,
+                    help="is in single mode")
+parser.add_argument("-f", "--feedback",
+                    action="store_true",
+                    default=True,
+                    help="no feedback column in grading")
 
 args=parser.parse_args()
 
@@ -17,33 +31,34 @@ if len(sys.argv) == 1:
     print("use in folder where your Zip is located")
     sys.exit(1)
 
-    if args.grading != None:
-        GRADING_FILE = args.grading
-    else:
-        raise Exception
+if args.destroy[0] != None:
+    GRADING_FILE = args.destroy[0].name
+else:
+    raise Exception
 
-    if args.moodle != None:
-        MOODLE_EXPORT = args.moodle
-    else:
-        raise Exception
+if args.destroy[1] != None:
+    MOODLE_EXPORT = args.destroy[1].name
+else:
+    raise Exception
 
-    if args.result != None:
-        RESULT_FILE = args.result
-    else:
-        raise Exception
+if args.result[0] != None:
+    RESULT_FILE = args.result[0].name
+else:
+    raise Exception
+
 
 with open(GRADING_FILE, 'rU', newline='') as grading, \
-    open(MOODLE_EXPORT, 'rU', newline='') as moodle, \
-    open(RESULT_FILE, 'w', newline='') as result:
+     open(MOODLE_EXPORT, 'rU', newline='') as moodle, \
+     open(RESULT_FILE, 'w', newline='') as result:
     reader_grading = csv.DictReader(grading)
     reader_moodle = csv.DictReader(moodle)
 
-    header = reader_moodle.fieldnames 
+    header = reader_moodle.fieldnames
     writer = csv.DictWriter(result, \
                             header, \
                             quotechar='"', \
                             quoting=csv.QUOTE_NONNUMERIC)
-    writer.writeheader()   
+    writer.writeheader()
     moodlelist = []
     gradinglist = []
 
@@ -56,6 +71,6 @@ with open(GRADING_FILE, 'rU', newline='') as grading, \
         for row in moodlelist:
             #print(line['Gruppe'],"\nrow:",row['Gruppe'])
             if line['Gruppe'] == row['Gruppe']:
-                    row['Bewertung'] = line['Bewertung']
-                    row['Feedback als Kommentar'] = line['Feedback als Kommentar']
-                    writer.writerow(row)
+                row['Bewertung'] = line['Bewertung']
+                row['Feedback als Kommentar'] = line['Feedback als Kommentar']
+                writer.writerow(row)
