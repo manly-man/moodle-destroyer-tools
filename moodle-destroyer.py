59374a7a479b27075466f918dc214711fb94230b
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2015-05-19 19:30:29 +0200

    add argumentparser

--- moodle-destroyer.py
+++ moodle-destroyer.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
 import sys
 import csv
+import argparse
+
+parser = argparse.ArgumentParser(prog="Moodle Destroyer", prefix_chars="-")
+
+parser.add_argument("-v", "--version", help="WE ARE TALKING ABOUT THE VERSION")
+parser.add_argument("-g", "--grading", help="ALL YOUR GRADINGS BELONG TO US")
+parser.add_argument("-m", "--moodle", help="gimme the moodle stuff")
+parser.add_argument("-r", "--result", help="name your result")
+
+args=parser.parse_args()
 
 if len(sys.argv) == 1:
     print("Usage: moodle-destroyer moodle-file grading-file output-file")
     print("use in folder where your Zip is located")
     sys.exit(1)
 
-MOODLE_EXPORT = sys.argv[1]
-GRADING_FILE = sys.argv[2]
-RESULT_FILE = sys.argv[3]
+    if args.grading != None:
+        GRADING_FILE = args.grading
+    else:
+        raise Exception
+
+    if args.moodle != None:
+        MOODLE_EXPORT = args.moodle
+    else:
+        raise Exception
+
+    if args.result != None:
+        RESULT_FILE = args.result
+    else:
+        raise Exception
 
 with open(GRADING_FILE, 'rU', newline='') as grading, \
     open(MOODLE_EXPORT, 'rU', newline='') as moodle, \
