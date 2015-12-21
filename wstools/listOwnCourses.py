770bd8d8fdff8600f052e165527539da85b68e86
David Schmid <info@david-schmid.de>
2015-12-21 09:58:13 +0100

    sorted output by id

--- wstools/listOwnCourses.py
+++ wstools/listOwnCourses.py
@@ -32,10 +32,14 @@ request2 = requests.post(url, postData2)
 
 courses = json.loads(request2.text)
 
+clist = []
 for course in courses:
     if 1 == course['visible']:
-        print(str(course['id']) + ' ' +course['fullname'])
-
+        clist.append([course['id'],course['fullname']])
+#        print(str(course['id']) + ' ' +course['fullname'])
+clist.sort()
+for c in clist:
+    print(c)
 #functions = [func['name'] for func in wsFunc['functions']]
 #for function in sorted(functions):
 #    print(function)
