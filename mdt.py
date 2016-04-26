ee9a2b8e6afd56970e6c298838fe6f5c38c6c41a
David Schmid <info@david-schmid.de>
2016-04-26 17:46:43 +0200

    find work_tree config and subcmd

--- mdt.py
+++ mdt.py
@@ -1,10 +1,45 @@
 #!/usr/bin/env python3
+import glob
 import os
+import subprocess
+import configargparse
 
-def find_subcmd(program):
-    return program
+
+def find_external_subcmds():
+    exec_paths = os.get_exec_path()
+    exec_paths += '.'  # TODO remove temp fix until mdt is in $PATH
+    # print('looking for executables in:\n' + '\n'.join(exec_paths))
+    mdt_executables = []
+    for path in exec_paths:
+        mdt_executables += glob.glob(path + '/mdt-*')
+    return mdt_executables
 
 
-path = os.environ['PATH'].split(os.pathsep)
+def find_global_config_file():
+    if 'XDG_CONFIG_HOME' in os.environ:
+        if os.path.isfile(os.environ['XDG_CONFIG_HOME']+'/mdtconfig'):
+            return os.environ['XDG_CONFIG_HOME']+'/mdtconfig'
+    elif os.path.isfile(os.path.expanduser('~/.mdtconfig')):
+        return os.path.expanduser('~/.mdtconfig')
+    else:
+        return None
 
-print(path)
+
+def find_work_tree():
+    cwd = os.getcwd()
+    repo = None
+    while not os.path.isdir('.mdt'):
+        if '/' == os.getcwd():
+            break
+        os.chdir(os.pardir)
+    if os.path.isdir('.mdt'):
+        repo = os.getcwd()
+    os.chdir(cwd)
+    return repo
+
+
+print(find_external_subcmds())
+print(find_work_tree())
+print(os.getcwd())
+
+
