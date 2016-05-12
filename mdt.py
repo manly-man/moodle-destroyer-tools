e0fc7eb5d7477a4ab644ec65e3ee2a98b747e5f4
David '-1' Schmid <david-1.schmid@uni-ulm.de.de>
2016-05-12 14:45:53 +0200

    mdt can now access external commands

--- mdt.py
+++ mdt.py
@@ -4,23 +4,25 @@ import os
 import subprocess
 import configargparse
 import re
+import sys
 
+INSTALL_PATH = '/home/bone/projects/moodle-destroyer-tools-pycharm'
 
 def find_external_subcmds():
     """this gets all files that are in $PATH and match mdt-*
 
     :returns dict with subcmd:full_path
     """
+    # TODO has problems with sub-sub-commands, mtd-init-full will override mdt-init
     exec_paths = os.get_exec_path()
-    exec_paths += '.'  # TODO remove temp fix until mdt is in $PATH
+    exec_paths.append('.')   # TODO remove temp fix until mdt is in $PATH
     mdt_executable_paths = []
     for path in exec_paths:
         mdt_executable_paths += glob.glob(path + '/mdt-*')
     mdt_pattern = re.compile('mdt-\w+')
     matches = mdt_pattern.findall("".join(mdt_executable_paths))  # find all like mdt-*
     cmd_names = [re.compile('^mdt-').sub('', m) for m in matches]  # strip ^mdt-
-    subcmds = dict(zip(cmd_names, mdt_executable_paths))
-    return subcmds
+    return dict(zip(cmd_names, mdt_executable_paths))
 
 
 def find_global_config_file():
@@ -50,29 +52,67 @@ def find_work_tree():
     os.chdir(cwd)
     return repo
 
+
+def check_for_sub_command():
+    if 1 >= len(sys.argv):
+        return None
+    else:
+        return sys.argv[1]
+
+
+def execute_external(sub_command):
+    argv = sys.argv[1:]
+    argv[0] = sub_command
+    print('executing '+repr(argv))
+    subprocess.run(argv)
+
+
+def find_internal_cmd():
+    pass
+
+
 global_config = find_global_config_file()
 if global_config is None:
     print('you should have rolled. try: \n  mdt init')  # TODO help sheeples.
-    exit(-1)
+    exit(1)
 
+configs = [global_config]
 work_tree = find_work_tree()
-p=[]
 if work_tree is not None:
-    print()
-    p = configargparse.getArgumentParser(name='mdt', default_config_files=[global_config, work_tree+'/.mdt/config'])
+    # default_config_files order is crucial: work_tree cfg overrides global
+    configs.append(work_tree+'/.mdt/config')
+
+p = configargparse.getArgumentParser(name='mdt', default_config_files=configs)
+
+ext_sub_commands = find_external_subcmds()
+sub_command = check_for_sub_command()
+
+
+if sub_command is None:
+    print('you should give a subcommand, i know these:')
+    [print('  ' + cmd) for cmd in ext_sub_commands.keys()]
+    exit(1)
+elif sub_command in ext_sub_commands:
+    execute_external(ext_sub_commands[sub_command])
 else:
-    p = configargparse.getArgumentParser(name='mdt', default_config_files=[global_config])
+    print('i don\'t know of this subcommand. I know those:\n')
+    [print('  ' + cmd) for cmd in ext_sub_commands.keys()]
+    exit(1)
 
-p.add_argument('--uid')
-p.add_argument('--url')
-p.add_argument('--token')
-p.add_argument('--user')
+#print(find_global_config_file())
+#argv = sys.argv
+#print(ext_sub_commands)
+#print(find_work_tree())
+#print(os.getcwd())
+#print(p)
+
+#p.add_argument('subcommand', help='subcommand to use', action='store', nargs='?', type=str)
+#p.add_argument('--uid')
+#p.add_argument('--url')
+#p.add_argument('--token')
+#p.add_argument('--user')
 
 
-opt = p.parse_args()
-print(find_global_config_file())
-print(find_external_subcmds())
-print(find_work_tree())
-print(os.getcwd())
-print(p)
-print(opt)
+#opt = p.parse_args()
+
+
