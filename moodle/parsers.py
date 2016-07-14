810b7b0d74bc1930aca47de7b68201e779f23565
David '-1' Schmid <david-1.schmid@uni-ulm.de.de>
2016-07-14 12:06:53 +0200

    parser for url2metadata

--- moodle/parsers.py
+++ moodle/parsers.py
@@ -11,21 +11,25 @@ def strip_mlang(string, preferred_lang='en'):
     lang_regex = re.compile(r'\{mlang\s*(\w{2})\}')
     lang_set = set(lang_regex.findall(string))
 
+    # if there is more than one language, discard all but preferred_lang
     if len(lang_set) > 1:
         lang_set.discard(preferred_lang)  # removes preferred lang from set, langs in set will be purged
         discard_mlang = '|'.join(lang_set)
         pattern = re.compile(r'((?=\{mlang ('+discard_mlang+r')\})(.*?)\{mlang\})+?', flags=re.S)
         string = pattern.sub('', string)
 
+    # remove remaining mlang tags.
     strip_mlang = re.compile(r'(\s*\{mlang.*?\}\s*)+?')
     return strip_mlang.sub('', string)
 
-# this is for getting file metadata like size and such.
-# comp = re.compile(r'.*pluginfile.php'
-#                   r'/(?P<context_id>[0-9]*)'
-#                   r'/(?P<component>\w+)'
-#                   r'/(?P<file_area>\w+)'
-#                   r'/(?P<item_id>[0-9]*).*')
-# match = comp.match(url)
+
+def file_meta_dict_from_url(url):
+    comp = re.compile(r'.*pluginfile.php'
+                      r'/(?P<context_id>[0-9]*)'
+                      r'/(?P<component>\w+)'
+                      r'/(?P<file_area>\w+)'
+                      r'/(?P<item_id>[0-9]*).*')
+    match = comp.match(url)
+    return match.groupdict()
 # print(wsfunc.get_file_meta(options, **match.groupdict()))
 
