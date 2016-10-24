74ae5081ae832313d05341b21904e04cdfd355d5
David '-1' Schmid <david-1.schmid@uni-ulm.de.de>
2016-10-24 13:52:46 +0200

    moodle 3.1 WS API

---
+++ moodle_api_3.1/get_api.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import requests
+
+from bs4 import BeautifulSoup as BS
+from bs4 import SoupStrainer as SS
+
+
+url = 'https://demo.moodle.net'
+ws_api = '/admin/webservice/documentation.php'
+login = '/login/index.php'
+logindata = {'username': 'admin','password': 'sandbox'}
+
+def main():
+
+    s = requests.Session()
+
+    #get cookies
+    r = s.post(url + login, data=logindata)
+    r = s.get(url+ws_api)
+
+    main_strain = SS(id='region-main')
+    main_soup = BS(r.text, 'html.parser', parse_only=main_strain)
+
+    parse_api(main_soup)
+    return main_soup
+
+def parse_api(soup):
+    cmd_divs = soup.find_all(attrs={'class':'collapsibleregion collapsed'})
+    for cmd_div in cmd_divs:
+        title_soup = cmd_div.div.find_all('div', class_='collapsibleregioncaption')
+        func_name = title_soup[0].strong.contents[0]
+        func_soup = cmd_div.div.find_all('div', class_='collapsibleregioninner')
+        func_description = func_soup[0].div.contents[0]
+        with open(func_name + '.html', 'w') as f:
+            f.write(str(func_soup[0]))
+
+
+if __name__ == '__main__':
+    print(main())
+
